f53f0bb7428695909994c676971ba9d85ea3b1d2
liutao <acompanion1992@gmail.com>
2020-08-25 21:49:02 +0800

    solve 0094

---
+++ 0094-BinaryTreeInorderTraversal/src/Solution.java
@@ -0,0 +1,40 @@
+/**
+ * 给定一个二叉树，返回它的中序 遍历。
+ *
+ * 示例:
+ *
+ * 输入: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
+ * 输出: [1,3,2]
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-inorder-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+class Solution {
+
+    List<Integer> list = new ArrayList<>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        inorder(root);
+        return list;
+    }
+
+    private void inorder(TreeNode node) {
+
+        if (node == null)
+            return;
+        list.add(node.val);
+        inorder(node.left);
+        inorder(node.right);
+    }
+}
